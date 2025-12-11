c33beed384dc3a6931d147bbcea0297f7a1a1d1e
artemsmirnov <learningartems@gmail.com>
2025-12-11 23:27:16 +0200

    Lint added

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,6 @@ dependencies {
     implementation("io.ktor:ktor-server-status-pages")
     implementation("io.ktor:ktor-server-cio:3.1.1")
 
-
     testImplementation(platform("org.junit:junit-bom:5.12.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation(kotlin("test"))
